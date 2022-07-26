6585e758e3aaec5fa0cb90b0733f9e2479ff85a3
Angel Ortiz <107958827+alortizOQ@users.noreply.github.com>
2022-07-26 03:23:51 -0500

    Add files via upload
    New updates to navbar and content on the Hero part of the website

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Hero = () => {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[96px] w-full h-screen mx-auto text-center flex flex-col justify-center px-8">
+        <p className="text-[#00df9a] font-bold p-2">
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+          GROW WITH DATA.
+        </h1>
+        <div className="flex justify-center items-center">
+          <p className="md:text-4xl sm:text-2xl text-xl font-bold py-4">
+            Fast, flexible financing for
+          </p>
+          <Typed
+            className="md:text-4xl sm:text-2xl text-xl font-bold md:pl-4 pl-2"
+            strings={["BTB", "BTC", "SASS"]}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
+        </div>
+        <p className="md:text-2xl text-xl font-bold text-gray-500">Monitor your data analytics to increase revenue for BTB, BTC, & SASS plataforms.</p>
+        <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black">Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
